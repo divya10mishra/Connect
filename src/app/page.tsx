afc0af74ebbfc3d3f6a15362834ec899d6988dad
divya_mishra <divyamishra075@gmail.com>
2023-12-16 19:08:54 +0530

    'initial-structure'

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Main(){
+  return(
+      <div className='layout-design'>
+      <aside>chat room</aside>
+      <main>
+          <header>header</header>
+          <section>video screen</section>
+          <footer>activity section</footer>
+      </main>
+      
+      </div>
+  )
+ }
+ 
+ export default Main
---
+++ src/components/main.tsx
@@ -0,0 +1,14 @@
+function Main(){
+ return(
+     <>
+     <aside>chat room</aside>
+     <main>
+         <header>header</header>
+         <article>video screen</article>
+         <footer>activity section</footer>
+     </main>
+     </>
+ )
+}
+
+export default Main
